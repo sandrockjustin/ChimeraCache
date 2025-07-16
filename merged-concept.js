c4b4834a0ef8559f934bb6a51722a6f2f1d68cda
sandrockjustin <sandrock.justin@gmail.com>
2025-07-16 18:32:42 -0500

    FIX / continued refinement, feel like there is a better way to do constructor

--- merged-concept.js
+++ merged-concept.js
@@ -3,55 +3,86 @@ const path = require('path');
 
 class ChimeraCache {
 
-  #manifest;
-  #dirname;
-  #automatic_startup;
-  #invalidation_protocol;
-  #fallback;
+  #storage = {};
+  #overrides
+  #invalidation
+  #fallback
   #subroutine = null;
-  #custom_subroutine;
   #sys_mem = os.totalmem();
 
-  constructor(options = { 'fallback': {}}) {
+  constructor(options = {}) {
 
-    if (options.fallback) {
-      this.#fallback = {
-      'enabled': options.fallback.enabled ? options.fallback.enabled : true,
-      'mode': options.fallback.mode ? options.fallback.mode: 'hybrid',
-      'threshold': options.fallback.threshold ? options.fallback.threshold : 0.8,
-      }
+    if (this.#overrides.ignore_defaults) {
+      const {overrides, invalidation, fallback} = options;
+
+      this.#overrides = overrides;
+      this.#invalidation = invalidation;
+      this.#fallback = fallback;
+
+      this.init();
     } else {
-      this.#fallback = {
-        'enabled': true,
-        'mode': 'hybrid',
-        'threshold': 0.8,
-        'activated': false
+
+      this.#overrides = {
+        dirname: options.overrides.dirname ? options.overrides.dirname : path.resolve(),
+        automatic_startup: options.overrides.automatic_startup ? options.overrides.automatic_startup : true,
+        ignore_defaults: options.overrides.ignore_defaults ? options.overrides.ignore_defaults : false
+      }
+
+      this.#invalidation = {
+        protocol: options.invalidation.protocol ? options.invalidation.protocol.toLowerCase() : 'lru',
+        caching: {
+          limit: {
+            enabled: options.invalidation.caching.limit.enabled ? options.invalidation.caching.limit.enabled : false,
+            max : options.invalidation.caching.limit.max ? options.invalidation.caching.limit.max : 0
+          },
+          bytes: {
+            enabled:  options.invalidation.bytes.enabled ? options.invalidation.bytes.enabled : true, 
+            ratio: options.invalidation.bytes.ratio ? options.invalidation.bytes.ratio : 0.1
+          }
+        },
+        ttl: {
+          enabled: options.invalidation.ttl.enabled ? options.invalidation.ttl.enabled : true,
+          max: options.invalidation.ttl.max ? options.invalidation.ttl.max : 900000,
+          min: options.invalidation.ttl.min ? options.invalidation.ttl.min : 300000,
+          extension: options.invalidation.ttl.extension ? options.invalidation.ttl.extension : (expires_at) => expires_at + 5000
+        }
       };
+
+      this.#fallback = {
+        enabled: options.fallback.enabled ? options.fallback.enabled : true,
+        protocol: options.fallback.protocol ? options.fallback.protocol.toLowerCase() : 'hybrid',
+        threshold: options.fallback.threshold ? options.fallback.threshold : 0.8,
+        activated: false // should never be changed by the user, never allowed
+      };
+
     }
-
-    this.#manifest = options.manifest_override ? options.manifest_override : {};
-    this.#dirname = options.dirname ? options.dirname : path.resolve();
-    this.#automatic_startup = options.automatic_startup ? options.automatic_startup : true;
-    this.#invalidation_protocol = options.invalidation_protocol ? options.invalidation_protocol.toLowerCase() : 'lru';
-    this.#custom_subroutine = options.custom_subroutine ? options.custom_subroutine : null;
-
   }
 
-  sanitize_cache_key(x) {
+  sanitize(x) {
     return x.replace(/[<>:"/\\|?*\x00-\x1F]/g, '_')
             .replace(/[\s.]+$/g, ''); 
   }
 
   invalidate(cache_key) {
-    delete this.#manifest[cache_key]
+    delete this.#storage[cache_key]
   }
 
   get(cache_key) {
-    return this.#manifest[cache_key].data;
+    return this.#storage[this.sanitize(cache_key)].data;
+  }
+
+  set(cache_key, data) {
+
+    const key = this.sanitize(cache_key);
+
+    this.#storage[key] = new CacheElement({
+      cache_key: key,
+      data: data
+    })
   }
 
   // System check on memory to see if fallback protocol is necessary
-  sys_mem_check(){
+  #sys_mem_check(){
     const unalloc_mem = os.freemem();
     const alloc_mem = (this.#sys_mem - unalloc_mem);
     const percent_mem = alloc_mem / this.#sys_mem;
@@ -62,7 +93,7 @@ class ChimeraCache {
   }
 
   // Initiated if a system memory check determines that fallback protocol must activate; stops all subroutines
-  sys_mem_fallback(sys_info) {
+  #sys_mem_fallback(sys_info) {
     console.log(`→ System Memory Usage: ${sys_info.alloc_mem}/${this.#sys_mem} (${(sys_info.percent_mem * 100).toFixed(2)}%) has exceeded fallback policy threshold of (${(this.#fallback.threshold * 100).toFixed(2)}%).`)
 
     if (!this.#fallback.enabled) {
@@ -76,36 +107,31 @@ class ChimeraCache {
     }
   }
 
-  sys_start_subroutines() {
-    switch (this.#invalidation_protocol) {
+  start_subroutines() {
+    switch (this.#invalidation.protocol) {
       case ('lru'):
         this.sys_lru_subroutine();
       case ('static_expiration'):
         this.sys_static_subroutine();
       case ('dynamic_expiration'):
         this.sys_dynamic_subroutine();
-      case ('custom'):
-        this.sys_custom_subroutine();
       default:
         this.sys_lru_subroutine();
     }
   }
 
-  sys_lru_subroutine() {
+  #lru_subroutine() {
   }
 
-  sys_static_subroutine() {
+  #static_subroutine() {
   }
 
-  sys_dynamic_subroutine() {
-  }
-
-  sys_custom_subroutine() {
+  #dynamic_subroutine() {
   }
 
 }
 
-class Element {
+class CacheElement {
 
   constructor(options = {}){
     this.created_at = Date.now();
@@ -126,7 +152,7 @@ class Element {
   }
 
   // updates engagement for cache item, shows how recently accessed for LRU
-  update(options = {}) {
+  #update(options = {}) {
     this.engagement += 1;
     this.last_accessed = Date.now();
   }
