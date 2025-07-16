d173d161f348c04dd17db9bd4636c515c601471d
sandrockjustin <sandrock.justin@gmail.com>
2025-07-16 16:18:33 -0500

    FIX / various restructurings for stability and extensibility

--- merged-concept.js
+++ merged-concept.js
@@ -1,29 +1,53 @@
 const os = require('os');
+const path = require('path');
 
 class ChimeraCache {
 
-  constructor(options = {}) {
+  #manifest;
+  #dirname;
+  #automatic_startup;
+  #invalidation_protocol;
+  #fallback;
+  #subroutine = null;
+  #custom_subroutine;
+  #sys_mem = os.totalmem();
+
+  constructor(options = { 'fallback': {}}) {
+
+    if (options.fallback) {
+      this.#fallback = {
+      'enabled': options.fallback.enabled ? options.fallback.enabled : true,
+      'mode': options.fallback.mode ? options.fallback.mode: 'hybrid',
+      'threshold': options.fallback.threshold ? options.fallback.threshold : 0.8,
+      }
+    } else {
+      this.#fallback = {
+        'enabled': true,
+        'mode': 'hybrid',
+        'threshold': 0.8,
+        'activated': false
+      };
+    }
+
     this.#manifest = options.manifest_override ? options.manifest_override : {};
-    this.#invalidation_protocol = options.invalidation_protocol ? options.invalidation_protocol.toUpperCase() : 'LRU';
-    this.#fallback_protocol = options.fallback_protocol ? options.fallback_protocol : true;
-    this.#fallback_threshold = options.fallback_threshold ? options.fallback_threshold : 0.8;
-    this.#fallback_activated = false;
+    this.#dirname = options.dirname ? options.dirname : path.resolve();
+    this.#automatic_startup = options.automatic_startup ? options.automatic_startup : true;
+    this.#invalidation_protocol = options.invalidation_protocol ? options.invalidation_protocol.toLowerCase() : 'lru';
+    this.#custom_subroutine = options.custom_subroutine ? options.custom_subroutine : null;
+
   }
 
-  #manifest = {};
-  #invalidation_protocol = "lru";
-  #fallback_protocol = false;
-  #fallback_threshold = 0.8;
-  #fallback_activated = false;
-  #subprocess = null;
-  #sys_mem = os.totalmem();
+  sanitize_cache_key(x) {
+    return x.replace(/[<>:"/\\|?*\x00-\x1F]/g, '_')
+            .replace(/[\s.]+$/g, ''); 
+  }
 
   invalidate(cache_key) {
     delete this.#manifest[cache_key]
   }
 
-  invalidateAll() {
-
+  get(cache_key) {
+    return this.#manifest[cache_key].data;
   }
 
   // System check on memory to see if fallback protocol is necessary
@@ -32,24 +56,51 @@ class ChimeraCache {
     const alloc_mem = (this.#sys_mem - unalloc_mem);
     const percent_mem = alloc_mem / this.#sys_mem;
 
-    if (percent_mem >= this.#fallback_threshold) {
-      sys_mem_fallback();
+    if (percent_mem >= this.#fallback.threshold) {
+      sys_mem_fallback({alloc_mem, percent_mem});
     }
   }
 
+  // Initiated if a system memory check determines that fallback protocol must activate; stops all subroutines
   sys_mem_fallback(sys_info) {
-    console.log(`→ System Memory Usage: ${sys_info.alloc_mem}/${this.#sys_mem} (${(sys_info.percent_mem * 100).toFixed(2)}%) has exceeded fallback policy threshold of (${(this.#fallback_threshold * 100).toFixed(2)}%).`)
+    console.log(`→ System Memory Usage: ${sys_info.alloc_mem}/${this.#sys_mem} (${(sys_info.percent_mem * 100).toFixed(2)}%) has exceeded fallback policy threshold of (${(this.#fallback.threshold * 100).toFixed(2)}%).`)
 
-    if (!this.#fallback_protocol) {
-      console.error(`→ Warning: Fallback protocol is disabled (false) and will not be executed.`);
+    if (!this.#fallback.enabled) {
+      console.error(`→ Warning: Fallback protocol is disabled (false) and will not be executed. Current memory usage has accepted acceptable threshold; performance may be affected on your system.`);
     } else {
-
 
       /**
        * Write code to begin hybrid-caching mode with Node file system
        */
 
     }
+  }
+
+  sys_start_subroutines() {
+    switch (this.#invalidation_protocol) {
+      case ('lru'):
+        this.sys_lru_subroutine();
+      case ('static_expiration'):
+        this.sys_static_subroutine();
+      case ('dynamic_expiration'):
+        this.sys_dynamic_subroutine();
+      case ('custom'):
+        this.sys_custom_subroutine();
+      default:
+        this.sys_lru_subroutine();
+    }
+  }
+
+  sys_lru_subroutine() {
+  }
+
+  sys_static_subroutine() {
+  }
+
+  sys_dynamic_subroutine() {
+  }
+
+  sys_custom_subroutine() {
   }
 
 }
@@ -78,7 +129,6 @@ class Element {
   update(options = {}) {
     this.engagement += 1;
     this.last_accessed = Date.now();
-    options.invalidation_protocol(options.arguments);
   }
 
 }
