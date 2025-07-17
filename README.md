ff6fe0fd7e487588f3aeb3bee99e40af93a4b0fd
sandrockjustin <sandrock.justin@gmail.com>
2025-07-17 16:00:06 -0500

    FIX / restructure and begin update README

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# local_caching
-Experimenting with local caching solutions in non-volatile (and volatile) memory for more cost-effective solutions in specific environments.
+# About ChimeraCache
+
+ChimeraCache is an experimental package which aims to provide a configurable, easy-to-use caching solution for developers. As a package, ChimeraCache enables developers to quickly establish volatile caching with non-volatile fallback caching using the Node.js File System module. ChimeraCache monitors your system's performance, as well as its own performance as a process, to determine when fallback caching should activate. In the event that the non-volatile fallback does not meet your needs, you may prescribe your own custom fallback procedures to offload caching to a remote solution or another process of your choosing.
+
+## Features
+
+ChimeraCache aims to be highly configurable, and will update to include quality of life features recommended by the community. The current version which is under production (v1) will allow for developers to set the following configurations:
+| Setting | Feature | Feature Description |
+| ------------ | -------------------- | ----------------------------------------------------------------------------------------------------------------------------------------------- |
+| Invalidation | Main Protocol | Alternate between `static, dynamic, LRU, custom` protocols to handle the invalidation of cached items on a defined subroutine. |
+| Invalidation | TTL | Establish a minimum TTL that will be used to determine when an item should be removed from the cache. Optionally establish a maximum TTL. <br> |
+| Invalidation | TTL - Extend | Optionally create a custom function that receives item details, and updates the current TTL for a cache item up to (not exceeding) maximum TTL. |
+| Caching | Size Constraints | Limit the cache to retain a maximum specified quantity, useful for LRU fine-tuning. |
+| Caching | Bytesize Constraints | Set a minimum (and maximum) bytesize allowed for caching in volatile storage. |
+| Caching | Bytesize Ratios | Set a minimum (and maximum) bytesize percentage compared to system memory or process memory. |
+| Caching | Overflow Protocol | Items exceeding bytesize constraints (or ratios) will be stored using the fallback policy for a hybrid caching approach. |
+
+## Fallback Protocols
+
+The fallback protocol chosen by your development team should reflect the needs of your application, as well as the hardware that will be hosting your server. ChimeraCache will constantly assess your system's available memory, in addition to the memory being used by ChimeraCache as a process. By default, ChimeraCache has fallback enabled and will initiate fallback protocols when system memory usage remains between the `threshold_min` and `threshold_max` for approximately 30 seconds.
+
+- Flex: The `flex` protocol attempts to balance performance needs according to your system's memory usage. Upon reaching or exceeding the `fallback threshold`,
+- Orphan:
+- Foreign: The `foreign` protocol allows for end-users to establish their own fallback protocol, as well as `getForeign()` and `setForeign()`. This may be used to offload some caching of ChimeraCache's responsibilities to foreign infrastructures such as [Redis, etc.]
---
+++ archive/v3_07_16_2025.js
@@ -0,0 +1,172 @@
+const os = require('os');
+const path = require('path');
+
+class ChimeraCache {
+
+  #storage = {};
+  #overrides
+  #invalidation
+  #fallback
+  #subroutine = null;
+  #sys_mem = os.totalmem();
+
+  constructor(options = {}) {
+
+    if (this.#overrides.ignore_defaults) {
+      const {overrides, invalidation, fallback} = options;
+
+      this.#overrides = overrides;
+      this.#invalidation = invalidation;
+      this.#fallback = fallback;
+
+      this.init();
+    } else {
+
+      this.#overrides = {
+        dirname: options.overrides.dirname ? options.overrides.dirname : path.resolve(),
+        automatic_startup: options.overrides.automatic_startup ? options.overrides.automatic_startup : true,
+        ignore_defaults: options.overrides.ignore_defaults ? options.overrides.ignore_defaults : false
+      }
+
+      this.#invalidation = {
+        protocol: options.invalidation.protocol ? options.invalidation.protocol.toLowerCase() : 'static',
+        caching: {
+          limit: {
+            enabled: options.invalidation.caching.limit.enabled ? options.invalidation.caching.limit.enabled : false,
+            max : options.invalidation.caching.limit.max ? options.invalidation.caching.limit.max : 0
+          },
+          bytes: {
+            enabled:  options.invalidation.bytes.enabled ? options.invalidation.bytes.enabled : true, 
+            ratio: options.invalidation.bytes.ratio ? options.invalidation.bytes.ratio : 0.1
+          }
+        },
+        ttl: {
+          enabled: options.invalidation.ttl.enabled ? options.invalidation.ttl.enabled : true,
+          max: options.invalidation.ttl.max ? options.invalidation.ttl.max : 900000,
+          min: options.invalidation.ttl.min ? options.invalidation.ttl.min : 300000,
+          extension: options.invalidation.ttl.extension ? options.invalidation.ttl.extension : (expires_at) => expires_at + 5000
+        }
+      };
+
+      this.#fallback = {
+        enabled: options.fallback.enabled ? options.fallback.enabled : true,
+        protocol: options.fallback.protocol ? options.fallback.protocol.toLowerCase() : 'flex', 
+        threshold_min: options.fallback.threshold_min ? options.fallback.threshold_min : 0.7,
+        threshold_max: options.fallback.threshold_max ? options.fallback.threshold_max : 0.85,
+        activated: false, // should never be changed by the user, never allowed
+      };
+
+      this.init();
+    }
+  }
+
+  #init() {
+
+
+
+  }
+
+  sanitize(x) {
+    return x.replace(/[<>:"/\\|?*\x00-\x1F]/g, '_')
+            .replace(/[\s.]+$/g, ''); 
+  }
+
+  invalidate(cache_key) {
+    delete this.#storage[cache_key]
+  }
+
+  get(cache_key) {
+    return this.#storage[this.sanitize(cache_key)].data;
+  }
+
+  set(cache_key, data) {
+
+    const key = this.sanitize(cache_key);
+
+    this.#storage[key] = new CacheElement({
+      cache_key: key,
+      data: data
+    })
+  }
+
+  // System check on memory to see if fallback protocol is necessary
+  #sys_mem_check(){
+    const unalloc_mem = os.freemem();
+    const alloc_mem = (this.#sys_mem - unalloc_mem);
+    const percent_mem = alloc_mem / this.#sys_mem;
+
+    if (percent_mem >= this.#fallback.threshold) {
+      sys_mem_fallback({alloc_mem, percent_mem});
+    }
+  }
+
+  // Initiated if a system memory check determines that fallback protocol must activate; stops all subroutines
+  #sys_mem_fallback(sys_info) {
+    console.log(`→ System Memory Usage: ${sys_info.alloc_mem}/${this.#sys_mem} (${(sys_info.percent_mem * 100).toFixed(2)}%) has exceeded fallback policy threshold of (${(this.#fallback.threshold * 100).toFixed(2)}%).`)
+
+    if (!this.#fallback.enabled) {
+      console.error(`→ Warning: Fallback protocol is disabled (false) and will not be executed. Current memory usage has accepted acceptable threshold; performance may be affected on your system.`);
+    } else {
+
+      /**
+       * Write code to begin hybrid-caching mode with Node file system
+       */
+
+    }
+  }
+
+  start_subroutines() {
+    switch (this.#invalidation.protocol) {
+      case ('lru'):
+        this.sys_lru_subroutine();
+      case ('static_expiration'):
+        this.sys_static_subroutine();
+      case ('dynamic_expiration'):
+        this.sys_dynamic_subroutine();
+      default:
+        this.sys_lru_subroutine();
+    }
+  }
+
+  #lru_subroutine() {
+  }
+
+  #static_subroutine() {
+  }
+
+  #dynamic_subroutine() {
+  }
+
+}
+
+class CacheElement {
+
+  constructor(options = {}){
+    this.created_at = Date.now();
+    this.expires_at = this.created_at + options.protocol_base_constraint;
+    this.expires_at_max = this.created_at + options.protocol_max_constraint;
+    this.last_accessed = Date.now();
+    this.engagement = 1;
+    this.cache_key = options.cache_key;
+    this.data = options.data;
+  }
+
+  // when invoked, used to invalidate this item in cache
+  invalidate(cache) {
+    if (this.cache_key) {
+      delete cache[this.cache_key];     // deletes from cache
+      cache.invalidate(this.cache_key); // deletes from manifest
+    }
+  }
+
+  // updates engagement for cache item, shows how recently accessed for LRU
+  #update(options = {}) {
+    this.engagement += 1;
+    this.last_accessed = Date.now();
+  }
+
+}
+
+const test = new Cache();
+
+test.sys_mem_check();
